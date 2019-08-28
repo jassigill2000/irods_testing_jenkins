e46c4fc8805a0a032105c62b4a69a431f179e779
Jaspreet Gill <jessi.k.gill@gmail.com>
2019-08-28 11:52:44 -0400

    changes made for externals

--- irods_docker_files/install_externals.py
+++ irods_docker_files/install_externals.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 
 import irods_python_ci_utilities
 
+def get_externals_directory():
+    return '/irods_externals' + irods_python_ci_utilities.get_irods_platform_string()
+
 def install_externals():
     irods_python_ci_utilities.install_os_packages([
          "irods-externals-avropre190cpp17-0",

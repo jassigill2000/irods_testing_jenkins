efc0017d410c9ee91f6578181cb45b864ee86ba1
Jaspreet Gill <jessi.k.gill@gmail.com>
2019-12-13 14:28:38 -0500

    [#29] changes made to check if containers are alive

--- irods_docker_files/setup_topo.py
+++ irods_docker_files/setup_topo.py
@@ -7,18 +7,17 @@ import sys
 import irods_python_ci_utilities
 import subprocess
 import shutil
-import ci_utilities
+import socket
 import time
+import ci_utilities
+import enable_ssl
 from subprocess import Popen, PIPE
 
 def get_irods_packages_directory():
     return '/irods_build/' + irods_python_ci_utilities.get_irods_platform_string()
 
-def setup_irods(database_type):
-    if database_type == 'postgres':
-        p = subprocess.check_call(['python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input'], shell=True)
-
 def check_ports_open(machine_name):
+    print(machine_name)
     listen_cmd = ['nc', '-vz', machine_name, '1247']
     status = 'refused'
     while status == 'refused':
@@ -34,14 +33,26 @@ def check_ports_open(machine_name):
 
     return status
 
-def setup_consumer():
-    check_ports_open('icat.example.org')
-    p = subprocess.check_call(['python /var/lib/irods/scripts/setup_irods.py < /tmp/irods_consumer.input'], shell=True)
+def set_univmss():
+    irods_directory = '/var/lib/irods'
+    univmss_testing = os.path.join(irods_directory, 'msiExecCmd_bin', 'univMSSInterface.sh')
+    if not os.path.exists(univmss_testing):
+        univmss_template = os.path.join(irods_directory, 'msiExecCmd_bin', 'univMSSInterface.sh.template')
+        with open(univmss_template) as f:
+            univmss_contents = f.read().replace('template-','')
+        with open(univmss_testing, 'w') as f:
+            f.write(univmss_contents)
+        os.chmod(univmss_testing, 0o544)
 
-def connect_to_network(machine_name, alias_name, network_name):
-    network_cmd = ['docker', 'network', 'connect', '--alias', alias_name, network_name, machine_name]
-    proc = Popen(network_cmd, stdout=PIPE, stderr=PIPE)
-    _out, _err = proc.communicate()
+def setup_consumer():
+    status = check_ports_open('icat.example.org')
+    print("setup_consumer")
+    if status == 'open':
+        p = subprocess.check_call(['python /var/lib/irods/scripts/setup_irods.py < /irods_consumer.input'], shell=True)
+        #enable_ssl.enable_ssl('consumer')
+
+def setup_provider():
+    p = subprocess.check_call(['python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input'], shell=True)
     
 def run_tests(test_type, test_name):
     print("let's try to run tests")
@@ -51,55 +62,59 @@ def run_tests(test_type, test_name):
 def check_topo_state(machine_name):
     is_running = True
     while is_running:
-        cmd = ['docker', 'inspect', '--format', '{{.State.Running}}', machine_name]
+        cmd = ['ping', '-W', '10', '-c', '1', machine_name]
         proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
         output, err = proc.communicate()
-        if 'false' in output:
-            exit_code = ['docker', 'inspect', '--format', '{{.State.ExitCode}}', machine_name]
-            ec_proc = Popen(exit_code, stdout=PIPE, stderr=PIPE)
-            _out, _err = ec_proc.communicate()
-            _out_split = _out.split('/')
-            _ec = int(_out_split[0])
-            return _ec
-            sys.exit(_ec)
+        _rc = proc.returncode
+        if _rc != 0:
+            gather_logs()
+            sys.exit(_rc)
+
+def gather_logs():
+    output_directory = '/irods_test_env/{0}/{1}'.format(irods_python_ci_utilities.get_irods_platform_string(),socket.gethostname())
+    irods_python_ci_utilities.gather_files_satisfying_predicate('/var/lib/irods/log', output_directory, lambda x: True)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--is_consumer', action='store_true', default=False)
+    parser.add_argument('--is_provider', action='store_true', default=False)
     parser.add_argument('--consumer_name', type=str)
-    parser.add_argument('--provider_name', type=str, required=True)
+    parser.add_argument('--provider_name', type=str, required=False)
     parser.add_argument('-d', '--database_type', default='postgres', help='database type', required=True)
     parser.add_argument('--install_externals', action='store_true', default=False)
     parser.add_argument('--test_type', type=str)
     parser.add_argument('--test_name', type=str)
-    parser.add_argument('--network_name', type=str, required=True)
-    parser.add_argument('--alias_name', type=str, required=True)
+    parser.add_argument('--network_name', type=str, required=False)
+    parser.add_argument('--alias_name', type=str, required=False)
+    parser.add_argument('--consumer_list', type=str)
 
     args = parser.parse_args()
-   
+  
     distribution = irods_python_ci_utilities.get_distribution()
-    ci_utilities.install_irods_packages(args.database_type, args.install_externals, get_irods_packages_directory())
+    ci_utilities.install_irods_packages(args.database_type, args.install_externals, get_irods_packages_directory(), is_provider = args.is_provider)
 
-    if args.is_consumer:
-        connect_to_network(args.consumer_name, args.alias_name, args.network_name)
+    if not args.is_provider:
         setup_consumer()
-        if args.test_type == 'topology_resource':
+        if args.test_type == 'topology_resource' and args.alias_name == 'resource1.example.org':
             check_ports_open('icat.example.org')
             rc = run_tests(args.test_type, args.test_name)
             sys.exit(rc)
         else:
+            set_univmss()
             check_ports_open('icat.example.org')
-            check_topo_state(args.provider_name)
+            check_topo_state('icat.example.org')
     else:
-        connect_to_network(args.provider_name, args.alias_name, args.network_name)
-        setup_irods(args.database_type)
+        ci_utilities.setup_irods(args.database_type)
+        #enable_ssl.enable_ssl('provider')
+
         if args.test_type == 'topology_icat':
-            check_ports_open('resource1.example.org')
-            rc = run_tests(args.test_type, args.test_name)
-            sys.exit(rc)
+            status = check_ports_open('resource1.example.org')
+            if status == 'open':
+                rc = run_tests(args.test_type, args.test_name)
+                sys.exit(rc)
         else:
+            set_univmss()
             check_ports_open('resource1.example.org')
-            check_topo_state(args.consumer_name)
+            check_topo_state('resource1.example.org')
 
 
 if __name__ == '__main__':
